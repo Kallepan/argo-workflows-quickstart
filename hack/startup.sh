402b0229fa0a34dcd00ebaa9c9865588a1b1ac6d
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2024-09-18 14:23:49 +0200

    feat: Add workflow templates for Argo and FastSurfer

--- hack/startup.sh
+++ hack/startup.sh
@@ -2,6 +2,12 @@
 
 minikube start
 
-export ARGO_WORKFLOWS_VERSION=v3.5.10
-kubectl create namespace argo
-kubectl apply -n argo -f "https://github.com/argoproj/argo-workflows/releases/download/${ARGO_WORKFLOWS_VERSION}/quick-start-minimal.yaml"
+kubectl create ns argo
+kubectl create ns argo-events
+kubectl create ns kite-ference
+
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+
+helm install argo-events argo/argo-events -n argo-events
+helm install argo-workflows argo/argo-workflows -f values.yaml -n argo
